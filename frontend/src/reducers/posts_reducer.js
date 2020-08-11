b944b35c9dc66b3a1285f7f2ca44c92eea9187f8
Artem Silka <silkartem@gmail.com>
2020-08-11 14:50:13 -0400

    add posts reducer

---
+++ frontend/src/reducers/posts_reducer.js
@@ -0,0 +1,26 @@
+import {
+  RECEIVE_POSTS,
+  RECEIVE_USER_POSTS,
+  CREATE_POST
+} from "../actions/post_actions";
+
+const PostReducer = ( state, action ) => {
+  Object.freeze(state);
+  let newState = Object.assign({}, state);
+
+  switch (action.type) {
+    case RECEIVE_POSTS:
+      newState = action.posts.data;
+      return newState;
+    case RECEIVE_USER_POSTS:
+      newState = action.posts.data;
+      return newState;
+    case CREATE_POST:
+      newState = action.post.data;
+      return newState;
+    default:
+      return state;
+  }
+};
+
+export default PostReducer;
