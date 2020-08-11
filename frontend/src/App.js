0ecb74798bc48f5d47e61c4322b51fd731600109
Victor Crispin <victorrcrispinjr@gmail.com>
2020-08-11 14:08:05 -0400

    Fixed styling on website page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
 
-import configureStore from './store/store';
-import Root from './components/root';
+// import configureStore from './store/store';
+// import Root from './components/root';
 
 
 
-function App() {
-  return (
-    document.addEventListener('DOMContentLoaded', () => {
-      const root = document.getElementById('root');
-      let store = configureStore();
+// function App() {
+//   return (
+//     document.addEventListener('DOMContentLoaded', () => {
+//       const root = document.getElementById('root');
+//       let store = configureStore();
     
-      ReactDOM.render(<Root store={store}/>, root);
-    })
-  )
-}
+//       ReactDOM.render(<Root store={store}/>, root);
+//     })
+//   )
+// }
 
-export default App;
+// export default App;
