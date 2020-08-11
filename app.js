c916267194cf14db13181f545bb03ce04f1dc0f5
petiteoh <nikkiteoh@gmail.com>
2020-08-11 11:19:15 -0400

    heroku push

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const users = require("./routes/api/users");
 const bodyParser = require("body-parser");
 const User = require("./models/User");
 
+const path = require("path");
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+  app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  })
+}
 
 mongoose
   .connect(db, { useNewUrlParser: true })
