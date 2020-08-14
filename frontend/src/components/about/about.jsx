20951fccefc15307028328bde787b4b4c9c05c75
Victor Crispin <victorrcrispinjr@gmail.com>
2020-08-14 09:25:03 -0400

    Adding about, editting index title in head and adding about

---
+++ frontend/src/components/about/about.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "../../css/about/about.css";
+
+export default class About extends Component {
+  render() {
+    return (
+      <div className="about-container">
+        <h1>The Team</h1>
+      </div>
+    )
+  }
+}
