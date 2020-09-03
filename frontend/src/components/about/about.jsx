7dd02cf06b747c83b1f139499309ea17d9f16db8
vcjr <victorrcrispinjr@gmail.com>
2020-09-03 14:19:13 -0400

    extra changes

--- frontend/src/components/about/about.jsx
+++ frontend/src/components/about/about.jsx
@@ -5,13 +5,13 @@ export default class About extends Component {
   render() {
     return (
       <div className="about-container">
-        <h1>The Team</h1>
+        <h1 className="about-title">The Team</h1>
         <ul className="team-members">
           <li className="member">
             <img id="Nikki" src="https://medio-app-seed.s3.amazonaws.com/nikki-profile-img.jpg" alt="Nikki's Profile Image"/>
             <div className="member-info">
               <h3>Nikki</h3>
-              <h4>Team Lead</h4>
+              <h4 id="Nikki-title">Team Lead</h4>
               <a className="" href="https://github.com/petiteoh">Github</a>
               <a className="" href="https://www.linkedin.com/in/nikkiteoh/">LinkedIn</a>
               <a className="" href="https://angel.co/nikki-teoh">Angelist</a>
@@ -21,7 +21,7 @@ export default class About extends Component {
             <img id="Artem"  src="https://medio-app-seed.s3.amazonaws.com/artemprofileimage.png" alt="Artem's Profile Image"/>
             <div className="member-info">
               <h3>Artem</h3>
-              <h4>Flex Developer</h4>
+              <h4 id="Artem-title">Flex Developer</h4>
               <a className="" href="https://github.com/ArtemSilka">Github</a>
               <a className="" href="https://www.linkedin.com/in/artemsilka/">LinkedIn</a>
               <a className="" href="https://angel.co/u/artem-silka">Angelist</a>
@@ -31,7 +31,7 @@ export default class About extends Component {
             <img id="Cade"  src="https://medio-app-seed.s3.amazonaws.com/cade_prof_pic.jpg" alt="Cade's Profile Image"/>
             <div className="member-info">
               <h3>Cade</h3>
-              <h4>Back-end Developer</h4>
+              <h4 id="Cade-title">Back-end Developer</h4>
               <a className="" href="https://github.com/cschreger">Github</a>
               <a className="" href="https://www.linkedin.com/in/cade-schreger-01048aab/">LinkedIn</a>
               <a className="" href="https://angel.co/u/cade-schreger">Angelist</a>
@@ -41,7 +41,7 @@ export default class About extends Component {
             <img id="Victor" src="https://medio-app-seed.s3.amazonaws.com/victorcrispinprofile.jpg" alt="Victor's Profile Image"/>
             <div className="member-info">
               <h3>Victor</h3>
-              <h4>Font-end Developer</h4>
+              <h4 id="Cade-title">Font-end Developer</h4>
               <a className="" href="https://github.com/vcjr">Github</a>
               <a className="" href="https://www.linkedin.com/in/victorcrispin/">LinkedIn</a>
               <a className="" href="https://angel.co/u/victor-crispin">Angelist</a>
