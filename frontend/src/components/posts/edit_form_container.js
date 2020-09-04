15f53c7e1ee9a8cf5793046992f824eb181de7dd
petiteoh <nikkiteoh@gmail.com>
2020-09-04 19:47:17 -0400

    login form flash and edit form

--- frontend/src/components/posts/edit_form_container.js
+++ frontend/src/components/posts/edit_form_container.js
@@ -5,11 +5,18 @@ import EditForm from "./edit_form";
 const msp = (state, ownProps) => {
     debugger
     // const postId = ownProps.match.params.post_id
-    const post = Object.values(state.posts).filter(
-    (post) => post._id === ownProps.match.params.post_id)
+    const post = Object.values(state.posts).filter((post) => post._id === ownProps.match.params.post_id)
+    const categories = Object.values(state.categories).map((category) => category.name);
+    const categoryIds = Object.values(state.categories).map((category) => category._id);
+
     return {
         // postId,
+        currentUser: state.session.user,
         post,
+        categories,
+        categoryIds,
+        categoryObjs: Object.values(state.categories),
+        errors: state.errors.postErrors,
     };
 };
 
