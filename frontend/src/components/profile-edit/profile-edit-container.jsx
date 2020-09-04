54dab581153ab7ad10094af8a9be87f6108732de
vcjr <victorrcrispinjr@gmail.com>
2020-09-04 18:12:13 -0400

    Create edit profile modal container and component skeletons

---
+++ frontend/src/components/profile-edit/profile-edit-container.jsx
@@ -0,0 +1,16 @@
+import { connect } from "react-redux";
+import ProfileEdit from './profile-edit';
+
+const mapStatetoProps = (state) => {
+  return {
+
+  }
+};
+
+const mapDispatchtoProps = (dispatch) => {
+  return {
+
+  }
+};
+
+export default connect(mapStatetoProps, mapDispatchtoProps)(ProfileEdit);
---
+++ frontend/src/components/profile-edit/profile-edit.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProfileEdit extends Component {
+  render() {
+    return (
+      <div className="edit-profile-modal">
+        
+      </div>
+    )
+  }
+}
