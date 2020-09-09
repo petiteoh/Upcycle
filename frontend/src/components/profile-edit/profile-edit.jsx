291bdbd431b34c8de8e6b7e56d3b65f7cb649c1f
vcjr <victorrcrispinjr@gmail.com>
2020-09-09 10:44:28 -0400

    Add css to prodile edit form

--- frontend/src/components/profile-edit/profile-edit.jsx
+++ frontend/src/components/profile-edit/profile-edit.jsx
@@ -5,7 +5,7 @@ export default class ProfileEdit extends Component {
   constructor(props) {
     super(props);
 
-    debugger; // We might need to
+    // debugger; // We might need to
     this.state = {
       handle: this.props.user.handle,
       email: "Your Email",
@@ -44,15 +44,17 @@ export default class ProfileEdit extends Component {
               src={this.state.photo}
               alt={`${this.state.handle}'s profile image`}
             />
-            <label>
-              Handle
-              <input type="text" id="handle" value={this.state.handle} />
-            </label>
-            <label>
-              Email
-              <input type="text" id="email" value={this.state.email} />
-            </label>
-            <button>Save Profile</button>
+            <div className="edit-profile-input-container">
+              <label>
+                Handle
+                <input className="create-post-input oN bs" type="text" id="handle" value={this.state.handle} />
+              </label>
+              <label>
+                Email
+                <input className="create-post-input oN bs" type="text" id="email" value={this.state.email} />
+              </label>
+              <button className="create-post-button ready">Save Profile</button>
+            </div>
           </form>
         </div>
       </div>
